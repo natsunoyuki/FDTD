96b20f8ee1029723efb7260a3957051480610909
Юукий Нацумэ <33706936+natsunoyuki@users.noreply.github.com>
2022-09-18 13:05:04 +0900

    Update fdtd1d_laser.py

--- src/fdtd/fdtd1d_laser.py
+++ src/fdtd/fdtd1d_laser.py
@@ -116,7 +116,7 @@ class fdtd1d_laser(object):
 
     def run(self, n_iter = 10000, initiate_pulse = False, verbose = False):
         # Main FDTD Loop
-        for n in tqdm.trange(n_iter, disable = np.logical_not(verbose))
+        for n in tqdm.trange(n_iter, disable = np.logical_not(verbose)):
             # Update P
             self.P[self.die1:self.die2] = 1.0 / self.c1 * (self.c2 * self.P[self.die1:self.die2] - self.c3 * self.Pold[self.die1:self.die2] - self.c4 * self.E_y[self.die1:self.die2] * self.D[self.die1:self.die2])
             self.Pold = self.Place.copy() 
