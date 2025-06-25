17b75e6b61aed83f08a3fdeaa0a80ac8b38b5baa
Ненавин <33706936+natsunoyuki@users.noreply.github.com>
2025-06-25 12:33:59 +0800

    Added license text in source code files.

--- src/fdtd/__init__.py
+++ src/fdtd/__init__.py
@@ -1,4 +1,17 @@
-# __init__.py
+# Copyright 2025 Natsunoyuki.
+#
+# FDTD is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the Free 
+# Software Foundation, either version 3 of the License, or (at your option) any 
+# later version.
+#
+# FDTD is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+# details.
+#
+# You should have received a copy of the GNU General Public License along with 
+# FDTD. If not, see <https://www.gnu.org/licenses/>.
 
 from .fdtd1d import fdtd1d
 from .fdtd1d_laser import fdtd1d_laser
